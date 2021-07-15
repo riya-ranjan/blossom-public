8fd5dd880879a2805a74d6d57a3ec8343485971d
Riya Ranjan <riya.ranjan.00@gmail.com>
2021-07-14 18:47:20 -0700

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import pathlib
 from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='blossompy',
     version='0.1',
     ##packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='Necessary ',
+    description='Used to run and create basic commands for Human-Robot Interaction with the Blossom Robot',
     long_description=open('README.txt').read(),
     install_requires=['numpy'],
     url='https://github.com/riya-ranjan/blossom-public',
